5dd210fb390ecee7422d2854334633b6d3e7be0a
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-10-12 16:42:54 -0500

    Create fileoverflow.py

---
+++ fileoverflow.py
@@ -0,0 +1,7 @@
+print("Before running you are acknowledging that this software will fill up your memory!")
+input("")
+os.system("shutdown -r")
+i = 0
+while True:
+  i += 1
+  os.system("echo Try stopping me (●'◡'●) > death-" + str(i) + ".txt")
